bb7dc02b6f40e097901ea614ab957826bc8ba755
Zihao Zhang <zzh8829@gmail.com>
2022-10-12 08:53:23 -0700

    fix

--- main.tf
+++ main.tf
@@ -1,40 +1,40 @@
-local {
+locals {
   port = var.port ? var.port : var.engine == "postgres" ? "5432" : "3306"
+  vpc_security_group_ids = concat(
+    var.vpc_security_group_ids,
+    module.security_group_vpc.security_group_id,
+    var.publicly_accessible ? module.security_group_public.security_group_id : []
+  )
 }
 
 module "db" {
   source  = "terraform-aws-modules/rds/aws"
   version = "4.1.3"
 
-  identifier                          = var.identifier
-  instance_use_identifier_prefix      = var.instance_use_identifier_prefix
-  allocated_storage                   = var.allocated_storage
-  storage_type                        = var.storage_type
-  storage_encrypted                   = var.storage_encrypted
-  kms_key_id                          = var.kms_key_id
-  replicate_source_db                 = var.replicate_source_db
-  license_model                       = var.license_model
-  replica_mode                        = var.replica_mode
-  iam_database_authentication_enabled = var.iam_database_authentication_enabled
-  domain                              = var.domain
-  domain_iam_role_name                = var.domain_iam_role_name
-  engine                              = var.engine
-  engine_version                      = var.engine_version
-  skip_final_snapshot                 = var.skip_final_snapshot
-  snapshot_identifier                 = var.snapshot_identifier
-  copy_tags_to_snapshot               = var.copy_tags_to_snapshot
-  final_snapshot_identifier_prefix    = var.final_snapshot_identifier_prefix
-  instance_class                      = var.instance_class
-  db_name                             = var.db_name
-  username                            = var.username
-  password                            = var.password
-  port                                = local.port
-
-  vpc_security_group_ids = concat(
-    var.vpc_security_group_ids,
-    module.security_group.security_group_id
-  )
-
+  identifier                             = var.identifier
+  instance_use_identifier_prefix         = var.instance_use_identifier_prefix
+  allocated_storage                      = var.allocated_storage
+  storage_type                           = var.storage_type
+  storage_encrypted                      = var.storage_encrypted
+  kms_key_id                             = var.kms_key_id
+  replicate_source_db                    = var.replicate_source_db
+  license_model                          = var.license_model
+  replica_mode                           = var.replica_mode
+  iam_database_authentication_enabled    = var.iam_database_authentication_enabled
+  domain                                 = var.domain
+  domain_iam_role_name                   = var.domain_iam_role_name
+  engine                                 = var.engine
+  engine_version                         = var.engine_version
+  skip_final_snapshot                    = var.skip_final_snapshot
+  snapshot_identifier                    = var.snapshot_identifier
+  copy_tags_to_snapshot                  = var.copy_tags_to_snapshot
+  final_snapshot_identifier_prefix       = var.final_snapshot_identifier_prefix
+  instance_class                         = var.instance_class
+  db_name                                = var.db_name
+  username                               = var.username
+  password                               = var.password
+  port                                   = local.port
+  vpc_security_group_ids                 = local.vpc_security_group_ids
   availability_zone                      = var.availability_zone
   multi_az                               = var.multi_az
   iops                                   = var.iops
